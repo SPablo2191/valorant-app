e491083039dfc1b54487ba8922b47064cfb08713
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-17 19:25:30 -0300

    feat: adding Home Page in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/primereact.min.css";                                       
 import "primereact/resources/themes/lara-light-indigo/theme.css";
+import { HomePage } from './pages/home/HomeComponent';
 
 function App() {
   return (
     <>
       <PrimeReactProvider>
+        <HomePage/>
       </PrimeReactProvider>
     </>
   )
