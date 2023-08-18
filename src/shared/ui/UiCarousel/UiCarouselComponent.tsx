9fbf085ab88cc18ac8766342eee45d228b358e5d
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-18 13:48:45 -0300

    feat: ui carousel component added

---
+++ src/shared/ui/UiCarousel/UiCarouselComponent.tsx
@@ -0,0 +1,23 @@
+import { Carousel, CarouselResponsiveOption } from 'primereact/carousel';
+export function CarouselEvent({ items, itemTemplate }: { items: any[], itemTemplate: any }) {
+    const responsiveOptions: CarouselResponsiveOption[] = [
+        {
+            breakpoint: '1199px',
+            numVisible: 1,
+            numScroll: 1
+        },
+        {
+            breakpoint: '991px',
+            numVisible: 2,
+            numScroll: 1
+        },
+        {
+            breakpoint: '767px',
+            numVisible: 1,
+            numScroll: 1
+        }
+    ];
+    return (
+        <Carousel value={items} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={itemTemplate} />
+    );
+}
