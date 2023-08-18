30fa89f19a41eb030f1f670f3ee0ba19fa72ea02
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-18 13:40:56 -0300

    feat: carousel in process

--- src/pages/home/HomeComponent.tsx
+++ src/pages/home/HomeComponent.tsx
@@ -1,7 +1,9 @@
+import { HomeCarousel } from "./components/HomeCarousel/HomeCarouselComponent";
 import { HomeHeader } from "./components/HomeHeader/HomeHeaderComponent";
 
 export function HomePage() {
     return (<>
         <HomeHeader />
+        <HomeCarousel />
     </>);
 }
