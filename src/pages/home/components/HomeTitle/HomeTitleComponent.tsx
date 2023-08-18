9279b004c9705b81c9eb4d93815bbe75fa844905
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-18 11:21:28 -0300

    styles: personalize home title

--- src/pages/home/components/HomeTitle/HomeTitleComponent.tsx
+++ src/pages/home/components/HomeTitle/HomeTitleComponent.tsx
@@ -1,7 +1,9 @@
-export function HomeTitle(){
+import './HomeTitleStyle.css'
+export function HomeTitle() {
     return (
         <>
-        <h1>Valorant</h1>
+            <h1 className="text-slate-800 main-title text-7xl dark:text-white"><span className='text-red-600'>V</span>alorant</h1>
+            <h2 className="text-slate-800 main-title text-4xl dark:text-white">One <span className='text-red-600'>More</span></h2>
         </>
     );
 }
