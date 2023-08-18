2d13376fe9e3dc6e93fea28a1cd04d611a22303f
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-18 11:59:08 -0300

    feat: home header component added

---
+++ src/pages/home/components/HomeHeader/HomeHeaderComponent.tsx
@@ -0,0 +1,10 @@
+import { HomeTitle } from "../HomeTitle/HomeTitleComponent";
+import { UiInputText } from "../../../../shared/ui/UiInputText/UiInputTextComponent";
+export function HomeHeader(){
+    return (
+        <>
+        <HomeTitle/>
+        <UiInputText/>
+        </>
+    );
+}
