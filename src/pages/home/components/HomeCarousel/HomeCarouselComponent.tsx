27088ca74fb2fd65090ac4f1b763b32b1879a192
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-18 13:49:04 -0300

    refactor: delete carousel primereact

--- src/pages/home/components/HomeCarousel/HomeCarouselComponent.tsx
+++ src/pages/home/components/HomeCarousel/HomeCarouselComponent.tsx
@@ -1,27 +1,11 @@
-import { Carousel, CarouselResponsiveOption } from 'primereact/carousel';
 
-export  function  HomeCarousel(){
-    const responsiveOptions: CarouselResponsiveOption[] = [
-        {
-            breakpoint: '1199px',
-            numVisible: 1,
-            numScroll: 1
-        },
-        {
-            breakpoint: '991px',
-            numVisible: 2,
-            numScroll: 1
-        },
-        {
-            breakpoint: '767px',
-            numVisible: 1,
-            numScroll: 1
-        }
-    ];
+export function HomeCarousel() {
     return (
         <div className="md:mt-12">
-        <h2 className="text-2xl md:text-4xl text-white">Ultimos Eventos</h2>
-        <hr  className="w-24 h-1 mx-auto  bg-red-600  border-0 rounded my-3"/>
+            <h2 className="text-2xl md:text-4xl text-white">Ultimos Eventos</h2>
+            <hr className="w-24 h-1 mx-auto  bg-red-600  border-0 rounded my-3" />
+
         </div>
     );
 }
+
