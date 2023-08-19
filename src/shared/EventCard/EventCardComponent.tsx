d456c2dbfa23910e4128e5d0f10b6b21948bb9d4
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-18 21:38:02 -0300

    fix: colors in cards

--- src/shared/EventCard/EventCardComponent.tsx
+++ src/shared/EventCard/EventCardComponent.tsx
@@ -3,13 +3,13 @@ import './EventCardStyle.css';
 
 export function EventCard({ event }: { event: Event }) {
     return (
-        <div className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5  mx-4 hover:bg-red-600 grow-on-hover  my-8 shadow-2xl">
+        <div className="bg-zinc-50 dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5  mx-4 hover:bg-red-600 grow-on-hover my-8 shadow-2xl">
             <div>
                 <span className="text-white font-bold inline-flex items-center justify-center p-2 bg-red-500 rounded-md shadow-lg">
                     <h2 >{event.shortDisplayName}</h2>
                 </span>
             </div>
-            <h3 className="text-slate-900 dark:text-white mt-5 text-2xl tracking-tight">{event.displayName}</h3>
+            <h3 className="text-slate-900 dark:text-white  mt-5 text-2xl tracking-tight">{event.displayName}</h3>
             <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
 
             </p>
--- src/shared/ui/UiInputText/UiInputTextComponent.tsx
+++ src/shared/ui/UiInputText/UiInputTextComponent.tsx
@@ -5,9 +5,9 @@ export function UiInputText({ placeholder}: { placeholder: string }) {
     const [value, setValue] = useState('');
     return (
         <div className="my-4">
-            <span className="p-input-icon-left">
+            <span className="p-input-icon-left ">
                 <i className="pi pi-search" />
-                <InputText placeholder={placeholder} className="bg-red-500 w-full border-none dark:bg-slate-600  dark:text-white md:w-96" value={value} onChange={(e) => setValue(e.target.value)} />
+                <InputText placeholder={placeholder} className="text-white bg-red-500 w-full border-none dark:bg-slate-600   md:w-96" value={value} onChange={(e) => setValue(e.target.value)} />
             </span>
         </div>
     );
