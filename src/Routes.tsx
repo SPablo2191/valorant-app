251ba1e99dc6c0ce43d300ada336fb6f6d28257d
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-19 12:55:17 -0300

    feat: error page added

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { HomePage } from './pages/home/HomeComponent'
 import { MainLayout } from './layouts/MainLayout/MainLayoutComponent'
+import { AgentPage } from './pages/agents/AgentComponent'
+import { ErrorPage } from './pages/error/ErrorComponent';
 
 export const router = createBrowserRouter([
     {
         path: '/', element: <MainLayout />,
         children: [
-            { path: '/', element: <HomePage /> }
+            { path: '/', element: <HomePage /> },
+            {path: '/agents',element : <AgentPage/>},
         ]
     },
-])
+    {path: '*', element: <ErrorPage/>}
+]);
