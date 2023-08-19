5a049cc2ed69cf6b0096cb8cbdef15255aa71147
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-19 10:55:44 -0300

    feat: footer component added

---
+++ src/shared/ui/UiFooter/UiFooterComponent.tsx
@@ -0,0 +1,7 @@
+export function UiFooter(){
+    return (<>
+    <div className="bg-slate-800 shadow-2xl">
+    <p>&copy; {new Date().getFullYear()} <span><a href="https://www.linkedin.com/in/pablo-sandoval-838553216/" target="_blank">Sandoval Pablo</a></span>. Todos los derechos reservados.</p>
+    </div>
+    </>);
+}
