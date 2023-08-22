dafeef8e9c7ae5c958d2e01ddcd6f35b7dbad2da
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-22 17:52:19 -0300

    fix: hook instead agent service

--- src/pages/agents/components/AgentCarousel/AgentCarouselComponent.tsx
+++ src/pages/agents/components/AgentCarousel/AgentCarouselComponent.tsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from "react";
-import { getAgents } from "../../../../services/AgentService";
 import { Agent } from "../../../../project/models/agent.interface";
 import { AgentCard } from "../../../../shared/components/AgentCard/AgentCardComponent";
 import { UiCarousel } from "../../../../shared/components/ui/UiCarousel/UiCarouselComponent";
+import { useModelList } from "../../../../shared/hooks/valorant.api.hook";
+import { pathnameEnum } from "../../../../project/interfaces/path.enum";
 
 export function AgentCarousel() {
-  const [data, setData] = useState<Agent[]>([]);
-  useEffect(() => {
-    getAgents().then((data) => {
-      console.log(data);
-      setData(data.data);
-    });
-  }, []);
+  const { data } = useModelList(
+    pathnameEnum.agents,
+    "?isPlayableCharacter=true"
+  );
   const itemTemplate = (item: Agent) => <AgentCard agent={item} />;
   return (
     <div className="mb-4">
