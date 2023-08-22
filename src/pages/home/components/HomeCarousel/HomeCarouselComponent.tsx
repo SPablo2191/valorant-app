01da60af076b1d5e5f6b52f5fee8dd1dd67e0a8f
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-22 17:59:53 -0300

    style: formating the code

--- src/pages/home/components/HomeCarousel/HomeCarouselComponent.tsx
+++ src/pages/home/components/HomeCarousel/HomeCarouselComponent.tsx
@@ -9,7 +9,9 @@ export function HomeCarousel() {
   const eventItemTemplate = (item: Event) => <EventCard event={item} />;
   return (
     <div className="md:mt-12">
-      <h2 className="text-2xl font-bold text-slate-800 dark:text-white md:text-4xl">Ultimos Eventos</h2>
+      <h2 className="text-2xl font-bold text-slate-800 dark:text-white md:text-4xl">
+        Ultimos Eventos
+      </h2>
       <hr className="w-24 h-1 mx-auto  bg-red-600  border-0 rounded my-3" />
       <div>
         <UiCarousel items={data} itemTemplate={eventItemTemplate} />
